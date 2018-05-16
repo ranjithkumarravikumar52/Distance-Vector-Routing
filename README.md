2ed14fb8094fc510562b4414e27c3d723ce18928
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-05-16 18:28:31 -0400

    Updates to readme

---
+++ README.md
@@ -0,0 +1,64 @@
+# Routing Information Protocol Java Application
+
+#### Introduction
+
+* Basic Information about Routing Information Protocol (RIP)? 
+    * Finding the least cost path between two nodes
+    * Implemented using Bellman-Ford's Algorithm
+    * Used in ARPAnet
+    * Distributed network
+#### Protocol Framework
+* Initial State at a node: 
+    * Cost (distance) to its **neighbors** is known
+* Final State at a node: 
+    * Cost to all the nodes is known, and also the **next hop**
+* Need to handle
+    * What information to exchange? 
+        * Each Node exchanges its routing table information only with it's neighbors
+        * Destination and Estimated cost to destination
+        * Next hop information is **NOT** shared
+        * HINT: use only destination and cost to send out the message
+    * How to act on message? 
+        * Update cost based on the bellman-ford's algorithm and change next hop accordingly
+    * When to send a message?
+        * Triggered Updates: Sent whenever there's a change in the routing table
+            * Link/Node failure or cost increase
+        * Periodic updates: 
+            * To say neighboring nodes ["I'm staying alive"](https://www.youtube.com/watch?v=I_izvAbhExY)
+* Each Node maintains its state through routing table (distance-vector)
+* Look at the below sample table
+
+| Destination        | Estimated Cost to Destination           | Next Hop Via to Reach Destination |
+| ------------- |:-------------:| -----:|
+| A     | 1 | A |
+| B     | 2      |   E |
+| C | 3      |    F |
+
+* After few message passing, 
+    * Nodes reach to a convergence, if there is no change in topology
+    * After one message exchange, each node knows about nodes two hops away
+    * After two message exchange, each node knows about nodes three hops away
+    * so..on
+    * Basically, converge will be reached in an atmost (V-1) times. where V: Nodes in the network (based on BF algorithm)
+* No node has GLOBAL knowledge
+* Fully distributed algorithm
+
+#### [Special Cases/Issues](https://www.youtube.com/watch?v=wDV0Nhs3_hE)
+* How are node/link failures detected? 
+    * Didn't receive periodic updates
+    * Can also actively probe (probe-ack)
+* Every path has a puddle (Counting to Infinity)
+    * Solutions
+        * Make infinity "small"
+        * Split horizon
+        * Split horizon with poison reverse
+        * Split horizon without poison reverse
+        * Hold-down timer
+        * 
+    
+    
+    
+#### Tutorials
+* [What is Routing Information Protocol](https://www.youtube.com/watch?v=dmS1t2twFrI)
+* [In-detail explanation on RIP](https://www.youtube.com/watch?v=-AyHRhaw4Ao)
+* [What are the special cases in RIP ?](https://www.youtube.com/watch?v=wDV0Nhs3_hE)
--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
-//name: Ranjith Kumar Ravikumar
-//ID: 800937157
+/**
+ * Name: Ranjith Kumar Ravikumar
+ */
 
 import java.io.*;
 
 public class App {
 
-    static int size_of_network;
-    static float[][] routing_table;
+    static int sizeOfNetwork;
+    static float[][] routingTable;
     static float infinity = Float.POSITIVE_INFINITY;
     static float[] vector, send_vector, receiving_vector;
     static float cost;
@@ -19,30 +20,30 @@ public class App {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input the total number of routers in the network N: ");
         try {
-            size_of_network = Integer.parseInt(br.readLine());
+            sizeOfNetwork = Integer.parseInt(br.readLine());
         } catch (Exception E) {
 
         }
 
         //creating the routing table
-        routing_table = new float[size_of_network][size_of_network];
+        routingTable = new float[sizeOfNetwork][sizeOfNetwork];
 
         //initializing infinity to all values in the table
-        for (int i = 0; i < size_of_network; i++) {
-            for (int j = 0; j < size_of_network; j++) {
-                routing_table[i][j] = infinity;
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            for (int j = 0; j < sizeOfNetwork; j++) {
+                routingTable[i][j] = infinity;
             }
         }
 
         //asking user to input values into the matrix
         try {
             System.out.println("---------Input the initial values into the routing table: Input -1 for whose costs are not known--------");
-            for (int i = 0; i < size_of_network; i++) {
-                for (int j = 0; j < size_of_network; j++) {
+            for (int i = 0; i < sizeOfNetwork; i++) {
+                for (int j = 0; j < sizeOfNetwork; j++) {
                     System.out.println("Enter value for " + i + " row " + " column " + j);
                     float temp = Float.parseFloat(br.readLine());
                     if (temp != -1) {
-                        routing_table[i][j] = temp;
+                        routingTable[i][j] = temp;
                     }
 
                 }
@@ -52,21 +53,21 @@ public class App {
         }
 
         //create vector matrix
-        vector = new float[size_of_network];
+        vector = new float[sizeOfNetwork];
 
         //debugging
-        //System.out.println("The size of nodes in N: "+size_of_network);
+        //System.out.println("The size of nodes in N: "+sizeOfNetwork);
         System.out.println();
-        System.out.println("-------------The values in the routing_table are------------- ");
-        for (int i = 0; i < size_of_network; i++) {
-            for (int j = 0; j < size_of_network; j++) {
-                System.out.print(" " + routing_table[i][j]);
+        System.out.println("-------------The values in the routingTable are------------- ");
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            for (int j = 0; j < sizeOfNetwork; j++) {
+                System.out.print(" " + routingTable[i][j]);
             }
             System.out.println();
         }
 
-        //update algo
-        //assuming the covergence reaches in size_of_network*size_of_network
+        //update algorithm
+        //assuming the convergence reaches in sizeOfNetwork*sizeOfNetwork
         message_passing();
         System.out.println();
         System.out.println("***********Convergence Reached in the Network***********");
@@ -83,12 +84,12 @@ public class App {
                 int change_column = Integer.parseInt(br.readLine());
                 System.out.println("Input the change in cost");
                 float new_cost = Float.parseFloat(br.readLine());
-                routing_table[change_row - 1][change_column - 1] = new_cost;
-                routing_table[change_column - 1][change_row - 1] = new_cost;
-                System.out.println("-------------The values in the routing_table after Link Cost Change------------- ");
-                for (int i = 0; i < size_of_network; i++) {
-                    for (int j = 0; j < size_of_network; j++) {
-                        System.out.print(" " + routing_table[i][j]);
+                routingTable[change_row - 1][change_column - 1] = new_cost;
+                routingTable[change_column - 1][change_row - 1] = new_cost;
+                System.out.println("-------------The values in the routingTable after Link Cost Change------------- ");
+                for (int i = 0; i < sizeOfNetwork; i++) {
+                    for (int j = 0; j < sizeOfNetwork; j++) {
+                        System.out.print(" " + routingTable[i][j]);
                     }
                     System.out.println();
                 }
@@ -111,69 +112,69 @@ public class App {
 
         //finding cost cost<- row2th value in row1
         //going to row2th element in row1
-        cost = routing_table[sending_row1 - 1][receiving_row2 - 1];
+        cost = routingTable[sending_row1 - 1][receiving_row2 - 1];
         System.out.println("Distance from sending router to receiving roouter " + cost);
 
         //adding cost to the sending_row2 vector and assign to temp vector
         //creating temp vector send_vector and receiving vector
-        temp = new float[size_of_network];
-        send_vector = new float[size_of_network];
-        receiving_vector = new float[size_of_network];
+        temp = new float[sizeOfNetwork];
+        send_vector = new float[sizeOfNetwork];
+        receiving_vector = new float[sizeOfNetwork];
 
         //assigning send_vector
-        for (int i = 0; i < size_of_network; i++) {
-            send_vector[i] = routing_table[sending_row1 - 1][i];
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            send_vector[i] = routingTable[sending_row1 - 1][i];
         }
 
         System.out.println();
 
         //assigning receiving_vector
-        for (int i = 0; i < size_of_network; i++) {
-            receiving_vector[i] = routing_table[receiving_row2 - 1][i];
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            receiving_vector[i] = routingTable[receiving_row2 - 1][i];
         }
 
         //adding cost
-        for (int i = 0; i < size_of_network; i++) {
+        for (int i = 0; i < sizeOfNetwork; i++) {
             temp[i] = cost + send_vector[i];
         }
 
         //comparing receiving vector and temp vector for min and assigning it to receiving vector
         //receiving vector <- min(receiving vector, temp_vector)
-        for (int i = 0; i < size_of_network; i++) {
+        for (int i = 0; i < sizeOfNetwork; i++) {
             //System.out.println("Comparing "+receiving_vector[i]+"  "+temp[i]);
             receiving_vector[i] = Math.min(receiving_vector[i], temp[i]);
         }
 
         //updating routing table with receiving vector
-        for (int i = 0; i < size_of_network; i++) {
-            routing_table[receiving_row2 - 1][i] = receiving_vector[i];
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            routingTable[receiving_row2 - 1][i] = receiving_vector[i];
         }
 
         //Debugging
         /*System.out.println("The values inside send_vector: ");
-		for(int i=0;i<size_of_network;i++){
+		for(int i=0;i<sizeOfNetwork;i++){
 			System.out.print(" "+send_vector[i]);
 		}
 		System.out.println();
 		System.out.println("The values inside receiving_vector ");
-		for(int i=0;i<size_of_network;i++){
+		for(int i=0;i<sizeOfNetwork;i++){
 			System.out.print(" "+receiving_vector[i]);
 		}
 		System.out.println();
 		System.out.println("The values inside the temp_vector ");
-		for(int i=0;i<size_of_network;i++){
+		for(int i=0;i<sizeOfNetwork;i++){
 			System.out.print(" "+temp[i]);
 		}
 		System.out.println();
 		System.out.println("-----Value of receiving vector after finding min------- ");
-		for(int i=0;i<size_of_network;i++){
+		for(int i=0;i<sizeOfNetwork;i++){
 			System.out.print(" "+receiving_vector[i]);
 		}
 		System.out.println();*/
-        System.out.println("-------------The values in the routing_table after message passing is------------- ");
-        for (int i = 0; i < size_of_network; i++) {
-            for (int j = 0; j < size_of_network; j++) {
-                System.out.print(" " + routing_table[i][j]);
+        System.out.println("-------------The values in the routingTable after message passing is------------- ");
+        for (int i = 0; i < sizeOfNetwork; i++) {
+            for (int j = 0; j < sizeOfNetwork; j++) {
+                System.out.print(" " + routingTable[i][j]);
             }
             System.out.println();
         }
@@ -181,7 +182,7 @@ public class App {
     }
 
     public static void message_passing() {
-        for (int i = 0; i < size_of_network * size_of_network; i++) {
+        for (int i = 0; i < sizeOfNetwork * sizeOfNetwork; i++) {
             //System.out.println("Inside the run loop: "+i);
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
